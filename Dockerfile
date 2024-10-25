d3b742d46a47cfc05b5a66ec9729c8e08a268ab2
Raffael <raffel_rocha@hotmail.com>
2024-10-26 01:04:38 +0200

    Fix in Dockerfile and setup.sh

--- Dockerfile
+++ Dockerfile
@@ -87,18 +87,6 @@ RUN apt-get update && \
     update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 50 && \
     update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 50
 
-COPY . /PFE
-
-# Setup Intel Pin
-RUN wget -P /opt http://software.intel.com/sites/landingpage/pintool/downloads/pin-3.15-98253-gb56e429b1-gcc-linux.tar.gz --no-check-certificate && \
-    tar -xf /opt/pin-3.15-98253-gb56e429b1-gcc-linux.tar.gz -C /opt && \
-    rm /opt/pin-3.15-98253-gb56e429b1-gcc-linux.tar.gz && \
-    chown -R PFE:PFE /opt/pin-3.15-98253-gb56e429b1-gcc-linux
-
-RUN bash scripts/setup.sh
-
-WORKDIR /PFE/
-
 # Create a user and group with the same UID and GID as the host user
 RUN groupadd -g $GID -o PFE \
     && useradd -u $UID -m -g PFE -G plugdev,sudo PFE
@@ -109,6 +97,18 @@ RUN echo "PFE ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/pfe && \
 
 # Change ownership of the alternatives directories to allow the user to switch GCC versions
 RUN chown -R PFE:PFE /var/lib/dpkg /var/lib/apt /var/cache/apt /var/log/apt /var/cache/debconf /etc/apt /etc/alternatives
+
+# Setup Intel Pin
+RUN wget -P /opt http://software.intel.com/sites/landingpage/pintool/downloads/pin-3.15-98253-gb56e429b1-gcc-linux.tar.gz --no-check-certificate && \
+    tar -xf /opt/pin-3.15-98253-gb56e429b1-gcc-linux.tar.gz -C /opt && \
+    rm /opt/pin-3.15-98253-gb56e429b1-gcc-linux.tar.gz && \
+    chown -R PFE:PFE /opt/pin-3.15-98253-gb56e429b1-gcc-linux
+
+COPY . /PFE
+
+WORKDIR /PFE/
+
+#RUN bash scripts/setup.sh
 
 # Switch to PFE user as the final step
 USER PFE
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,62 +1,113 @@
 #!/bin/bash
 
-# Exit on any error
-set -e
+# Exit on error, unset variable usage, or any failed pipeline
+set -euo pipefail
 
-# Delete all cloned submodules (if any)
-git submodule deinit -f .
+# Define versions of GCC
+GCC_VERSION_11="/usr/bin/gcc-11"
+GCC_VERSION_7="/usr/bin/gcc-7"
+GPP_VERSION_11="/usr/bin/g++-11"
+GPP_VERSION_7="/usr/bin/g++-7"
 
-# Delete gitignored files/folders
-git clean -Xdf
+# Number of processors (default to all if not defined)
+NPROC="${NPROC:-$(nproc)}"
 
-# Clone all submodules recursively
-git submodule update --init --recursive
+# Function to switch GCC versions
+switch_gcc_version() {
+    local gcc_version="$1"
+    local gpp_version="$2"
+    echo "Switching to GCC: $gcc_version and G++: $gpp_version"
+    update-alternatives --set gcc "$gcc_version"
+    update-alternatives --set g++ "$gpp_version"
+}
 
-# Apply patches to submodules
-for submodule in $(git submodule status | awk '{print $2}'); do
-    patches="patches/$submodule"
-    if [ -d "$patches" ]; then
-        for patch in "$patches"/*; do
-            patch=$(realpath $patch)
+# Function to apply patches to a submodule
+apply_patches() {
+    local submodule="$1"
+    local patches_dir="patches/$submodule"
+    if [ -d "$patches_dir" ]; then
+        for patch in "$patches_dir"/*; do
+            patch=$(realpath "$patch")
+            echo "Applying patch $patch to $submodule"
             (cd "$submodule" && git apply "$patch")
         done
     fi
-done
+}
 
-# ChampSim Configuration
-echo "###############    Starting ChampSim Configuration    ###############"
-update-alternatives --set gcc /usr/bin/gcc-11 
-update-alternatives --set g++ /usr/bin/g++-11
-cp -r Polices/hawkeye tools/ChampSim/replacement/
-cd tools/ChampSim 
-./vcpkg/bootstrap-vcpkg.sh 
-./vcpkg/vcpkg install 
-./config.sh champsim_config.json 
-make -j${nproc} -s
-cd tracer/cvp_converter
-g++ cvp2champsim.cc -o cvp_tracer 
-cd ../../..
+# Function to clean up git submodules
+clean_submodules() {
+    echo "Cleaning up all git submodules..."
+    git submodule deinit -f .
+    git clean -Xdf
+}
 
-# Intel PIN Configuration
-cd ChampSim/tracer/pin
-make -j${nproc} -s
-cd ../../../
+# Function to initialize and update submodules
+init_submodules() {
+    echo "Initializing and updating git submodules..."
+    git submodule update --init --recursive
+}
 
-# Scarab Configuration
-echo "###############    Starting Scarab Configuration    ###############"
-update-alternatives --set gcc /usr/bin/gcc-7 
-update-alternatives --set g++ /usr/bin/g++-7 
-cd scarab/src
-make -j${nproc} -s
-cd deps/dynamorio/
-cmake . && make -j32
-cd ../../../../
+# Function to configure and build ChampSim
+configure_champsim() {
+    echo "###############    Starting ChampSim Configuration    ###############"
+    switch_gcc_version "$GCC_VERSION_11" "$GPP_VERSION_11"
+    cp -r Polices/hawkeye tools/ChampSim/replacement/
+    cd tools/ChampSim
+    ./vcpkg/bootstrap-vcpkg.sh
+    ./vcpkg/vcpkg install
+    ./config.sh champsim_config.json
+    make -j"${NPROC}" -s
+    cd tracer/cvp_converter
+    g++ cvp2champsim.cc -o cvp_tracer
+    cd ../../..
+}
 
-### GEM5 Configuration
-echo "###############    Starting GEM5 Configuration    ###############"
-update-alternatives --set gcc /usr/bin/gcc-11 
-update-alternatives --set g++ /usr/bin/g++-11 
-cd gem5 
-echo | scons build/X86/gem5.opt -j${nproc}  # Skip the prompt
-#build/X86/gem5.opt configs/learning_gem5/part1/simple.py
-cd ../
+# Function to configure and build Intel PIN
+configure_intel_pin() {
+    echo "###############    Starting Intel PIN Configuration    ###############"
+    cd ChampSim/tracer/pin
+    make -j"${NPROC}" -s
+    cd ../../../
+}
+
+# Function to configure and build Scarab
+configure_scarab() {
+    echo "###############    Starting Scarab Configuration    ###############"
+    switch_gcc_version "$GCC_VERSION_7" "$GPP_VERSION_7"
+    cd scarab/src
+    make -j"${NPROC}" -s
+    cd deps/dynamorio/
+    cmake . && make -j"${NPROC}"
+    cd ../../../../
+}
+
+# Function to configure and build GEM5
+configure_gem5() {
+    echo "###############    Starting GEM5 Configuration    ###############"
+    switch_gcc_version "$GCC_VERSION_11" "$GPP_VERSION_11"
+    cd gem5
+    echo | scons build/X86/gem5.opt -j"${NPROC}"  # Skip the prompt
+    # Optionally, you can run a specific configuration by uncommenting the line below:
+    # build/X86/gem5.opt configs/learning_gem5/part1/simple.py
+    cd ..
+}
+
+# Main script execution
+main() {
+    clean_submodules
+    init_submodules
+
+    # Apply patches to all submodules
+    for submodule in $(git submodule status | awk '{print $2}'); do
+        apply_patches "$submodule"
+    done
+
+    # Execute configurations
+    configure_champsim
+    configure_intel_pin
+    configure_scarab
+    configure_gem5
+}
+
+# Run the main function
+main
